c504e769755250827694380c152799ec530df1b8
Tomas Pablo Korenblit <tomaskorenblit@gmail.com>
2021-12-17 20:19:07 -0300

    test

--- app.py
+++ app.py
@@ -1,46 +1,45 @@
-from flask import Flask, render_template, request, redirect, url_for
-import os
-from os.path import join, dirname, realpath
+from flask import Flask, json, render_template, request
+import json
 from numpy import int64
+import joblib
+import numpy as np
 import pandas as pd
+import re
+from nltk.corpus import stopwords
+
+import matplotlib.pyplot as plt
+
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict, StratifiedKFold
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import chi2, SelectKBest
+from sklearn.svm import LinearSVC
+from sklearn import metrics
+from sklearn.metrics import plot_confusion_matrix
+
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'files'
-app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
 
 @app.route("/")
 def index():
     
     return render_template("index.html")
-
-def parseCSV(filePath):
-    csvData = pd.read_csv(filePath)
-    csvData['Instagram id'] = csvData['Instagram id'].astype(str)
-    
-    df_scraped = pd.read_csv(r'scrapedId.csv',low_memory=False)
-    
-    csvData = csvData.loc[~csvData["Instagram id"].isin(df_scraped.Id),:]
-
-    userstoscrap = csvData.Username.values
-    return userstoscrap
-
-@app.route("/", methods=['POST'])
-def uploadFiles():
-    # get the uploaded file
-    uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-        # set the file path
-        uploaded_file.save(file_path)
-        userstoscrap = parseCSV(file_path)
-        userslen = len(userstoscrap)
-        userstoscrap = ('\n'.join('{}' for _ in range(len(userstoscrap))).format(*userstoscrap))
-        
-        # save the file
-        return render_template("index.html", filename=uploaded_file.filename, userstoscrap = userstoscrap, userslen=userslen)
-    else:
-        return render_template("index.html")
  
+@app.route('/predict', methods=['POST'])
+def predict():
+    json_ = request.json
+
+    p = re.compile(r'[^\w\s]+')
+
+    json_ = [p.sub('', x) for x in json_]
+    json_ = [each_string.lower() for each_string in json_]
+    
+    classifier = joblib.load('wellness-detection-model.pkl')
+    prediction = classifier.predict(json_)
+    return json.dumps(prediction.tolist())
+
 if __name__ == "__main__":
     app.run()
